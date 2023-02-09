bc4375c3dfebd1b65fd7cb75b238f983872f595e
Tibz-Dankan <tibesigwadankan@gmail.com>
2023-02-09 18:40:27 +0300

    feat: connected to mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,23 @@
 import { server } from "./app";
+import { AppError } from "./utils/error";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const startApp = () => {
+dotenv.config();
+const startApp = async () => {
   const port = process.env.PORT || 5000;
-  // connect mongo here
+  const MONGO_URI = process.env.MONGO_URI;
+
+  if (!MONGO_URI) return new AppError("MONGO URL not defined", 400);
+
+  try {
+    await mongoose.connect(MONGO_URI);
+
+    console.log("connected to mongo successfully!");
+  } catch (err: any) {
+    console.log(`An error occurred: ${JSON.stringify(err)}`);
+  }
+
   server.listen(port, () =>
     console.log(`server started and running on port ${port}...`)
   );
